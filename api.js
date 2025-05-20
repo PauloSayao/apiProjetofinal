393f32802237e701ce1373723e2b9dd46285aea7
Paulo Sayao <sayaopaulo@gmail.com>
2025-05-20 15:51:17 -0300

    resolvendo o problema

--- api.js
+++ api.js
@@ -11,13 +11,7 @@ const allowedOrigins = [
 const app = express();
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
@@ -31,6 +25,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
+
 
 app.use(express.static(path.join(__dirname)));
 app.options('*', cors());
